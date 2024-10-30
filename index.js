6ea7a7c7c933defaa95966bceca731b44ef8696a
bishal-codes <0karkibishal@gmail.com>
2024-10-30 18:05:05 +1100

    improved code and security risks

--- index.js
+++ index.js
@@ -1,93 +1,90 @@
 const display = document.querySelector(".display");
-// console.log("display:", { display });
+const buttons = document.querySelectorAll(".btns > div");
 
-const buttons = document.querySelectorAll(".btns > div"); // node list
-// console.log("buttons:", { buttons });
-
-const buttonsArray = Array.from(buttons); // array
-// console.log("buttonsArray:", buttonsArray);
-
-let displayStr = ""; // variable to store the display string
+const btnsArr = Array.from(buttons);
+let displayStr = "";
 
 let operators = ["+", "-", "*", "/"];
-
 let lastOperator = "";
 
 const displayResult = (displayStr) => {
-  // console.log("displayStr:", displayStr);
   display.textContent = displayStr || "0.00";
 };
 
-buttonsArray.forEach((button) => {
+btnsArr.forEach((button) => {
   button.addEventListener("click", (e) => {
-    // console.log("button clicked:", { button });
-    const buttonText = button.textContent;
-    // console.log("buttonText:", { buttonText });
+    const btnValue = button.textContent;
 
-    if (buttonText === "AC") {
+    if (btnValue === "AC") {
       displayStr = "";
       displayResult(displayStr);
       return;
     }
 
-    if (operators.includes(buttonText)) {
-      // includes returns true or false
-
-      if (displayStr === "") {
-        return;
-      }
-
-      lastOperator = buttonText;
-      //   console.log("lastOperator:", { lastOperator });
-      const lastChar = displayStr[displayStr.length - 1];
-      //   console.log("lastChar:", { lastChar });
-      if (operators.includes(lastChar)) {
-        displayStr = displayStr.slice(0, -1);
-        displayStr += buttonText;
-        displayResult(displayStr);
-        return;
-      }
-    }
-
-    if (buttonText === ".") {
-      if (displayStr.includes(".") && !lastOperator) {
-        return;
-      }
-      //  only one decimal point is allowed
-      // prevent multiple operators after the decimal point
-      if (lastOperator) {
-        const lastOperatorIndex = displayStr.lastIndexOf(lastOperator);
-        const lastNumber = displayStr.slice(lastOperatorIndex + 1);
-        console.log("lastNumber:", { lastNumber });
-        if (lastNumber.includes(".")) {
-          return;
-        }
-        if (!lastOperator && displayStr.includes(".")) {
-          return;
-        }
-      }
-    }
-
-    if (buttonText === "C") {
+    if (btnValue === "C") {
       displayStr = displayStr.slice(0, -1);
       displayResult(displayStr);
       return;
     }
 
-    if (buttonText === "=") {
-      if (displayStr === "") {
-        return;
+    if (operators.includes(btnValue)) {
+      const lastChar = displayStr[displayStr.length - 1]; // 123+
+      if (operators.includes(lastChar)) {
+        displayStr = displayStr.slice(0, -1);
       }
-
-      const result = eval(displayStr);
-      displayStr = result.toString();
+      displayStr += btnValue;
+      lastOperator = btnValue;
       displayResult(displayStr);
       return;
     }
 
-    displayStr += buttonText;
-    // console.log("displayStr:", displayStr);
+    if (btnValue === ".") {
+      // preventing multiple decimal points after an operator
+      if (lastOperator) {
+        const lastOperatorIndex = displayStr.lastIndexOf(lastOperator);
+        const lastNumber = displayStr.slice(lastOperatorIndex + 1);
+        if (lastNumber.includes(".")) {
+          return;
+        }
+      }
+      // prevent multiple decimal points before an operator
+      else if (displayStr.includes(".")) {
+        return;
+      }
+      // adding 0 BEFORE a decimal point if the display is empty
+      if (
+        displayStr === "" ||
+        operators.includes(displayStr[displayStr.length - 1])
+      ) {
+        displayStr += "0";
+      }
+    }
 
+    if (btnValue === "=") {
+      // displayStr = eval(displayStr).toString(); // leads to security issues
+
+      try {
+        const result = evalExpression(displayStr);
+        displayStr = result.toString();
+        displayResult(displayStr);
+      } catch (error) {
+        displayStr = "";
+        displayResult("Invalid expression");
+      }
+      return;
+    }
+
+    displayStr += btnValue;
     displayResult(displayStr);
   });
 });
+
+const evalExpression = (expression) => {
+  let result;
+  try {
+    result = new Function(`return ${expression}`)();
+  } catch (error) {
+    throw new Error("Invalid expression");
+  }
+  return parseFloat(result.toFixed(10));
+};
